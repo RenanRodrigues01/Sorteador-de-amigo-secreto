1cc29a021dce8f7d335dd2e8cbc5c938f950bb78
renan <renan.rodriguesmoura2022@gmail.com>
2023-02-15 11:46:46 -0300

    criando comportamento do formulario

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,26 @@
+import { useRef, useState } from "react"
 
 const Form = () => {
+
+  const [nome, setNome] = useState('');
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const adicionarParticipante = (evento: React.FormEvent<HTMLFormElement>) => {
+    evento.preventDefault();
+    setNome('');
+    inputRef.current?.focus()
+  }
+
   return (
-    <form>
-        <input type="text" placeholder="Insira os nomes dos participantes" />
-        <button disabled={true}>Adicionar</button>
+    <form onSubmit={adicionarParticipante}>
+        <input 
+          ref={inputRef}
+          value={nome}
+          onChange={e => setNome(e.target.value)}
+          type="text" 
+          placeholder="Insira os nomes dos participantes" 
+        />
+        <button disabled={!nome}>Adicionar</button>
     </form>
   )
 }
