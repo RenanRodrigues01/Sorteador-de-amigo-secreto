489cf7dafcaac1dd35b93b977e5546bfcd5c7ed8
renan <renan.rodriguesmoura2022@gmail.com>
2023-02-17 15:22:19 -0300

    testando navegação das paginas ao iniciar a brincadeira

--- src/components/Rodape.test.tsx
+++ src/components/Rodape.test.tsx
@@ -1,9 +1,25 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import React from "react";
 import { RecoilRoot } from "recoil";
+import { useListaDeParticipantes } from "../state/hooks/useListaDeParticipantes";
 import Rodape from "./Rodape";
 
-describe('onde não existem participantes suficientes' , () => {
+jest.mock('../state/hooks/useListaDeParticipantes', () => {
+    return {
+        useListaDeParticipantes: jest.fn()
+    }
+})
+const mockNavegação = jest.fn();
+
+jest.mock('react-router-dom', () => {
+    return {
+        useNavigate: () => mockNavegação
+    }
+} )
+describe('quando não existem participantes suficientes' , () => {
+    beforeEach(() => {
+        (useListaDeParticipantes as jest.Mock).mockReturnValue([])
+    })
     test('a brincadeira não pode ser iniciada com menos de 3 participantes', () => {
         render(
             <RecoilRoot>
@@ -15,4 +31,34 @@ describe('onde não existem participantes suficientes' , () => {
 
         expect(button).toBeDisabled()
     })
+})
+
+describe('quando existem participantes suficientes', () => {
+    beforeEach(() => {
+        (useListaDeParticipantes as jest.Mock).mockReturnValue(['josefina', 'maria', 'jose'])
+    })
+    test('A brincadeira pode ser iniciada', () => {
+        render(
+            <RecoilRoot>
+                <Rodape />
+            </RecoilRoot>
+        );
+
+        const button = screen.getByRole('button');
+
+        expect(button).not.toBeDisabled()
+    })
+
+    test('A brincadeira foi iniciada', () => {
+        render(
+            <RecoilRoot>
+                <Rodape />
+            </RecoilRoot>
+        );
+
+        const button = screen.getByRole('button');
+        fireEvent.click(button);
+
+        expect(mockNavegação).toHaveBeenCalledTimes(1)
+    } )
 })
