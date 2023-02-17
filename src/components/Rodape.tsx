38a4f0babfaa0748db232bbc5b296d5599bdd351
renan <renan.rodriguesmoura2022@gmail.com>
2023-02-17 15:25:19 -0300

    implementando navegação ao componente Rodape.tsx

--- src/components/Rodape.tsx
+++ src/components/Rodape.tsx
@@ -1,12 +1,20 @@
+import { useNavigate } from "react-router-dom";
 import { useListaDeParticipantes } from "../state/hooks/useListaDeParticipantes"
 
 const Rodape = () => {
-    const participantes = useListaDeParticipantes()
-  return (
-    <footer>
-        <button disabled={participantes.length < 3}>Iniciar a Brincadeira</button>
-    </footer>
-  )
-}
+    const participantes = useListaDeParticipantes();
+    const navigate = useNavigate()
+
+    const iniciar = () => {
+        navigate('/sorteioS')
+    }
+    return (
+        <footer>
+            <button disabled={participantes.length < 3} onClick={iniciar}>
+                Iniciar a Brincadeira
+            </button>
+        </footer>
+    )
+    }
 
 export default Rodape
