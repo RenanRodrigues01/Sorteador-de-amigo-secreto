1c162c2de81a3d74384397e67667c4fbab1931c0
renan <renan.rodriguesmoura2022@gmail.com>
2023-02-20 20:39:09 -0300

    criando estado para armezenar o resultado e implementando set dentro da função para guaradar efetivamente este resultado

--- src/state/atom.ts
+++ src/state/atom.ts
@@ -3,9 +3,14 @@ import {atom} from "recoil"
 export const listaDeparticipantesState = atom<string[]>({
     key: "listaDeparticipantes",
     default: []
-})
+});
 
 export const erroState = atom<string>({
     key: 'erroState',
     default: ""
+});
+
+export const resultadoAmigoSecretoState = atom<Map<string, string>>({
+    key: 'resultadoState',
+    default: new Map()
 })
