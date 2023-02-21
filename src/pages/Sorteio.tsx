1af2e675bdc9eafe62567d381ecb9b1ff5e7a475
renan <renan.rodriguesmoura2022@gmail.com>
2023-02-20 22:47:31 -0300

    testando se o amigo secreto está sendo exibido na tela

--- src/pages/Sorteio.tsx
+++ src/pages/Sorteio.tsx
@@ -5,8 +5,12 @@ const Sorteio = () => {
     const participantes = useListaDeParticipantes()
   return (
     <form>
-        <select name="participanteDaVez" id="participanteDaVez">
-            {participantes.map( participante => <option key={participante}>{participante}</option>)}
+        <select 
+          name="participanteDaVez" 
+          id="participanteDaVez"
+          placeholder="selecione o seu nome"
+        >
+          {participantes.map( participante => <option key={participante}>{participante}</option>)}
         </select>
     </form>
   )
