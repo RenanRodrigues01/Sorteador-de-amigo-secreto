4ee0cb0bfcfad29fdc805aedde98f29bd80bf011
renan <renan.rodriguesmoura2022@gmail.com>
2023-02-20 23:10:09 -0300

    exibindo o amigo secreto na tela

--- src/pages/Sorteio.tsx
+++ src/pages/Sorteio.tsx
@@ -1,18 +1,39 @@
+import { useState } from "react";
 import { useListaDeParticipantes } from "../state/hooks/useListaDeParticipantes"
+import { useResultadoSorteio } from "../state/hooks/useResultadoSorteio";
 
 
 const Sorteio = () => {
-    const participantes = useListaDeParticipantes()
+  const participantes = useListaDeParticipantes();
+
+  const [participanteDaVez, setPartipanteDaVez] =useState('');
+  const [amigoSecreto, setAmigoSecreto] = useState('');
+
+  const resultado = useResultadoSorteio();
+
+  const sortear = (evento: React.FormEvent<HTMLSelectElement>) => {
+    evento.preventDefault();
+    if(resultado.has(participanteDaVez)) {
+      setAmigoSecreto(resultado.get(participanteDaVez)!)
+    }
+  }
   return (
-    <form>
-        <select 
+    <section>
+      <form>
+        <select onSubmit={sortear}
           name="participanteDaVez" 
           id="participanteDaVez"
           placeholder="selecione o seu nome"
+          value={participanteDaVez}
+          onChange={e => setPartipanteDaVez( e.target.value)}
         >
           {participantes.map( participante => <option key={participante}>{participante}</option>)}
         </select>
-    </form>
+
+        <button>Sortear</button>
+      </form>
+      {amigoSecreto && <p role='alert'>{amigoSecreto}</p>}
+    </section> 
   )
 }
 
