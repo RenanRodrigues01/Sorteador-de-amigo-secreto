92ba4440ddfcdde983d25b8cb9b920a4234494e6
renan <renan.rodriguesmoura2022@gmail.com>
2023-02-22 12:42:35 -0300

    adicionando estilo a pagina de sorteio

--- src/pages/Sorteio.tsx
+++ src/pages/Sorteio.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
-import { useListaDeParticipantes } from "../state/hooks/useListaDeParticipantes"
+import Card from "../components/card/Card";
+import { useListaDeParticipantes } from "../state/hooks/useListaDeParticipantes";
 import { useResultadoSorteio } from "../state/hooks/useResultadoSorteio";
+
+import "./Sorteio.css"
 
 
 const Sorteio = () => {
@@ -18,22 +21,32 @@ const Sorteio = () => {
     }
   }
   return (
-    <section>
-      <form onSubmit={sortear}>
-        <select 
-          name="participanteDaVez" 
-          id="participanteDaVez"
-          placeholder="selecione o seu nome"
-          value={participanteDaVez}
-          onChange={e => setPartipanteDaVez( e.target.value)}
-        >
-          {participantes.map( participante => <option key={participante}>{participante}</option>)}
-        </select>
+    <Card>
 
-        <button>Sortear</button>
-      </form>
-      {amigoSecreto && <p role="alert">{amigoSecreto}</p>}
-    </section> 
+      <section className="sorteio">
+        <h2>Quem vai tirar o papelzinho?</h2>
+        <form onSubmit={sortear}>
+          <select 
+            name="participanteDaVez" 
+            id="participanteDaVez"
+            placeholder="selecione o seu nome"
+            value={participanteDaVez}
+            onChange={e => setPartipanteDaVez( e.target.value)}
+          >
+            {participantes.map( participante => <option key={participante}>{participante}</option>)}
+          </select>
+          <p>Clique em em sortear para ver quem é seu amigo secreto!</p>
+          <button className="botao-sortear">Sortear</button>
+        </form>
+
+        {amigoSecreto && <p className="resultado" role="alert">{amigoSecreto}</p>}
+
+        <footer className="sorteio">
+          <img src="/img/aviao.png" className="aviao" alt="Um desenho de um avião de papel" />
+        </footer>
+      </section> 
+      
+    </Card>
   )
 }
 
